fa7a31b923dde142a60e515fdb0819313922510f
harkirat2021 <71397998+harkirat2021@users.noreply.github.com>
2025-06-09 23:56:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -215,4 +215,5 @@ def inject_user():
     }
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+    app.run(host='0.0.0.0', debug=DEBUG) 
